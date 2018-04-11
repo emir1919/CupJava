e5f0cd3ca80ebce82295d4db69a02aa70997f70a
Souhail El Kamel <souhail.elkamel.1@esprit.tn>
2018-04-11 05:20:58 +0200

    Routes added

---
+++ src/edu/CupTest2/utils/Routes.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.cupcake.utils;
+
+/**
+ *
+ * @author El Kamel
+ */
+public class Routes {
+    
+    public static String AddAddsVIEW = "/edu/cupcake/gui/AddAdds.fxml";
+    public static String ShowAddsVIEW = "/edu/cupcake/gui/ShowAdds.fxml";
+    public static String EditAddsVIEW = "/edu/cupcake/gui/EditAdds.fxml";
+    public static String ShowClaimsAdminVIEW = "/edu/cupcake/gui/ShowClaimsAdmin.fxml";
+    public static String AddClaimsAdminVIEW = "/edu/cupcake/gui/AddClaims.fxml";
+    public static String ShowClaimsBrandVIEW = "/edu/cupcake/gui/ShowClaimsBrand.fxml";
+    public static String ShoweventsBrandVIEW = "/edu/cupcake/gui/ShoweventsBrand.fxml";
+    public static String AddEventsVIEW = "/edu/cupcake/gui/AddEvents.fxml";
+    public static String EditEventsVIEW = "/edu/cupcake/gui/EditEvents.fxml";
+    public static String ListEventsVIEW = "/edu/cupcake/gui/ListEvents.fxml";
+    public static String ListeParticipantsVIEW = "/edu/cupcake/gui/ListeParticipants.fxml";
+    public static String ConfirmationVIEW = "/edu/cupcake/gui/Confirmation.fxml";
+    public static String ErreurScanVIEW = "/edu/cupcake/gui/ErreurScan.fxml";
+    public static String ShowEventsFrontVIEW = "/edu/cupcake/gui/ShowEventsFront.fxml";
+    public static String ListeEventFrontVIEW = "/edu/cupcake/gui/ListeEventFront.fxml";
+    public static String ParticipateEventVIEW = "/edu/cupcake/gui/ParticipateEvent.fxml";
+    public static String ListeEventsPRVIEW = "/edu/cupcake/gui/ListeEventsPR.fxml";
+    public static String LOGIN = "/edu/cupcake/gui/Login.fxml";
+    public static String MesPointsView = "/edu/cupcake/gui/MesPoints.fxml";
+    public static String ConvertView = "/edu/cupcake/gui/Convert.fxml";
+
+
+
+    
+    
+    
+    
+
+    
+    
+
+    
+    
+
+    
+}
